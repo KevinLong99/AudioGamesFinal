db6b354a7aafd7863ece767cc052aaabf634c525
KevinLong99 <yl6260@nyu.edu>
2021-12-11 12:55:36 -0500

    may's commit

--- Assets/MayScripts/flute/fluteControl.cs
+++ Assets/MayScripts/flute/fluteControl.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
 public class fluteControl : MonoBehaviour
 {
+    public AudioMixerSnapshot normal;
+    public AudioMixerSnapshot reverb;
+    public AudioMixerSnapshot sad;
+    public AudioMixerSnapshot happy;
+    public AudioMixerSnapshot annoyed;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,10 @@ public class fluteControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            normal.TransitionTo(0);
+            Debug.Log("normal");
+        }
     }
 }
